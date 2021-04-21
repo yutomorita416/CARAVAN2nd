053172785d6f29dadc7c855fdcd8f7d55ad52ba2
yutomorita <yutomorita416@gmail.com>
2021-04-21 13:31:06 +0000

    finish 3-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,6 +5,7 @@ class BlogsController < ApplicationController
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
@@ -14,7 +15,7 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blog_path(blog.id)
   end
 
   def edit
@@ -24,7 +25,7 @@ class BlogsController < ApplicationController
   
   private
   def blog_params
-    params.require(:blog).permit(:title, :caegory, :body)
+    params.require(:blog).permit(:title, :category, :body)
   end
   
 end
